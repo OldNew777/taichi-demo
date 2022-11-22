d18c60923ecc6f5640d90c0a20991a6dec12e58e
chenxin <xin-chen22@mails.tsinghua.edu.cn>
2022-11-22 23:20:16 +0800

    bug: force calculation

--- mpm3d.py
+++ mpm3d.py
@@ -1,20 +1,25 @@
 import taichi as ti
 
 
+@ti.func
+def clamp(x, min_val, max_val):
+    return ti.max(min_val, ti.min(max_val, x))
+
+
 def mpm3d():
     ti.init(arch=ti.cpu)
 
     # rendering parameters
     resolution = (512, 512)
     n_steps = 50
-    len_grid = 64
+    n_grids = 64
     background_color = (0.2, 0.2, 0.4)
     point_color = (0.4, 0.6, 0.6)
-    point_radius = 0.005
+    point_radius = 0.002
 
     # constant physical values for simulation
-    n_particles = (len_grid ** 3) // 2
-    dx = 1. / len_grid
+    n_particles = (n_grids ** 3) // 4
+    dx = 1. / n_grids
     dt = 8e-5
     p_rho = 1.
     p_vol = (dx * 0.5) ** 3
@@ -24,21 +29,20 @@ def mpm3d():
     E = 400.
 
     # values for record
-    n_grid = len_grid ** 3
     x = ti.Vector.field(n=3, dtype=float, shape=(n_particles,))
     v = ti.Vector.field(n=3, dtype=float, shape=(n_particles,))
-    C = ti.Matrix.field(n=3, m=3, dtype=float, shape=(n_particles,))
     J = ti.field(dtype=float, shape=(n_particles,))
-    grid_v = ti.Vector.field(n=3, dtype=float, shape=(n_grid,))
-    grid_m = ti.field(dtype=float, shape=(n_grid,))
+    C = ti.Matrix.field(n=3, m=3, dtype=float, shape=(n_particles,))
+    grid_v = ti.Vector.field(n=3, dtype=float, shape=(n_grids, n_grids, n_grids))
+    grid_m = ti.field(dtype=float, shape=(n_grids, n_grids, n_grids))
 
     @ti.kernel
     def initialize():
-        for i in range(n_particles):
-            x[i] = [ti.random() * 0.4 + 0.2, ti.random() * 0.4 + 0.2, ti.random() * 0.4 + 0.2]
-            v[i] = [0., 0., 0.]
-            J[i] = 1.
-            C[i] = ti.Matrix.zero(float, 3, 3)
+        for index in ti.grouped(x):
+            x[index] = [ti.random() * 0.4 + 0.2, ti.random() * 0.4 + 0.2, ti.random() * 0.4 + 0.2]
+        v.fill([0., 0., 0.])
+        J.fill(1.)
+        C.fill(ti.Matrix.zero(float, 3, 3))
 
     @ti.kernel
     def clear_grid():
@@ -51,31 +55,64 @@ def mpm3d():
             Xp = x[index] / dx
             base = ti.cast(Xp - 0.5, ti.i32)
             fx = Xp - base.cast(float)
-            w = ti.Vector([0.5 * ((1.5 - fx) ** 2),
-                           0.75 - ((fx - 1) ** 2),
-                           0.5 * ((fx - 0.5) ** 2)])
-            stress = -4. * dt * E * p_vol * (J[index] - 1.) / (dx ** 2)
+            w_l = 0.5 * ti.pow((1.5 - fx), 2)
+            w_c = 0.75 - ti.pow((fx - 1), 2)
+            w_r = 0.5 * ti.pow((fx - 0.5), 2)
+            w = ti.Matrix.rows([w_l, w_c, w_r])
+            stress = -4. * dt * E * p_vol * (J[index] - 1.) / ti.pow(dx, 2)
             affine = ti.Matrix.one(float, 3, 3) * stress + p_mass * C[index]
-            for i, j, k in ti.ndrange(3, 3, 3):
-                offset = ti.Vector([i, j, k])
-                dpos = (offset.cast(float) - fx) * dx
-                weight = w[i][0] * w[j][1] * w[k][2]
-                v_add = weight * (p_mass * v[index] + affine.dot(dpos))
-                ti.atomic_add(grid_v[base + offset], v_add)
-                ti.atomic_add(grid_m[base + offset], weight * p_mass)
+            for i in ti.static(range(3)):
+                for j in ti.static(range(3)):
+                    for k in ti.static(range(3)):
+                        offset = ti.Vector([i, j, k], dt=int)
+                        dpos = (offset.cast(float) - fx) * dx
+                        weight = w[i, 0] * w[j, 1] * w[k, 2]
+                        v_add = weight * (p_mass * v[index] + affine @ dpos)
+                        ti.atomic_add(grid_v[base + offset], v_add)
+                        ti.atomic_add(grid_m[base + offset], weight * p_mass)
 
     @ti.kernel
     def simulate_grid():
-        for i in ti.grouped(grid_v):
-            pass
-            # if grid_m[i] > 0:
-            #     grid_v[i] = grid_v[i] / grid_m[i] + dt * gravity
-            # else:
-            #     grid_v[i] = [0., 0., 0.]
+        for i, j, k in ti.ndrange(grid_v.shape[0], grid_v.shape[1], grid_v.shape[2]):
+            m_temp = grid_m[i, j, k]
+            v_temp = ti.select(m_temp > 0, grid_v[i, j, k] / m_temp, ti.Vector.zero(float, 3))
+            # if m_temp <= 0:
+            #     print(i, j, k, m_temp, '<= 0 =', m_temp > 0, '; v_temp =', v_temp)
+            v_temp += dt * gravity
+            index = ti.Vector([i, j, k], dt=int)
+            # if (index < bound).any():
+            #     print(index, '<', bound, '=', index < bound, '; v_temp =', v_temp)
+            cond = (index < bound and v_temp < 0.) or (index > n_grids - bound and v_temp > 0.)
+            v_temp = ti.select(cond, 0., v_temp)
+            grid_v[i, j, k] = v_temp
 
     @ti.kernel
     def grid_to_point():
-        pass
+        for index in ti.grouped(x):
+            Xp = x[index] / dx
+            base = ti.cast(Xp - 0.5, ti.i32)
+            fx = Xp - base.cast(float)
+            w_l = 0.5 * ti.pow((1.5 - fx), 2)
+            w_c = 0.75 - ti.pow((fx - 1), 2)
+            w_r = 0.5 * ti.pow((fx - 0.5), 2)
+            w = ti.Vector([w_l.x, w_l.y, w_l.z,
+                           w_c.x, w_c.y, w_c.z,
+                           w_r.x, w_r.y, w_r.z])
+            new_v = ti.Vector.zero(float, 3)
+            new_C = ti.Matrix.zero(float, 3, 3)
+            for i in ti.static(range(3)):
+                for j in ti.static(range(3)):
+                    for k in ti.static(range(3)):
+                        offset = ti.Vector([i, j, k], dt=int)
+                        dpos = (offset.cast(float) - fx) * dx
+                        weight = w[i * 3 + 0] * w[j * 3 + 1] * w[k * 3 + 2]
+                        g_v = grid_v[base + offset]
+                        new_v += weight * g_v
+                        new_C += 4 * weight * g_v.outer_product(dpos) / ti.pow(dx, 2)
+            v[index] = new_v
+            x[index] += dt * v[index]
+            J[index] *= 1. + dt * new_C.trace()
+            C[index] = new_C
 
     def substep():
         clear_grid()

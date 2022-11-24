aa00397903e19f656504eeff44f48c2eba0217f6
chenxin <xin-chen22@mails.tsinghua.edu.cn>
2022-11-24 22:07:45 +0800

    add sphere; remove function 'reverse'

--- mpm3d.py
+++ mpm3d.py
@@ -12,6 +12,24 @@ import tqdm
 
 
 @ti.func
+def norm(x):
+    return ti.sqrt(x.dot(x))
+
+
+@ti.func
+def in_sphere(pos, center, radius):
+    distance = norm(pos - center)
+    return distance < radius
+
+
+@ti.func
+def normal_sphere(pos, center):
+    distance = norm(pos - center)
+    normal = ti.select(distance > 1e-5, (pos - center) / distance, ti.Vector.zero(float, 3))
+    return normal
+
+
+@ti.func
 def gaussian_random(sigma: float = 1.0, mu: float = 0.0):
     u1 = ti.random(dtype=float)
     u2 = ti.random(dtype=float)
@@ -31,14 +49,13 @@ class MPM3DSimulator:
         self.n_steps = 50
         self.n_grids = 64
         self.n_insert_per_step = 64
-        self.radius_insert_last = ti.field(dtype=float, shape=(1,))
         self.radius_insert = 0.03
-        self.radius_insert_delta = 0.05
-        self.pos_insert = ti.Vector([0.2, 0.95, 0.7])
-        self.v_init_insert = ti.Vector([1.5, -5.0, -1.2])
+        self.pos_insert = ti.Vector([0.2, 0.9, 0.7])
+        self.v_insert = ti.Vector([1.5, -5.0, -1.2])
 
         self.n_particles = (self.n_grids ** 3) // (2 ** 3)
-        self.n_valid = self.n_particles
+        self.n_particles_max = max(self.n_particles << 2, 1 << 20)
+        print(f'Particles: {self.n_particles}/{self.n_particles_max}')
 
         # constant physical values for simulation
         self.dx = 1. / self.n_grids
@@ -51,20 +68,16 @@ class MPM3DSimulator:
         self.E = 400.
 
         # values for record
-        self.x = ti.Vector.field(n=3, dtype=float, shape=(self.n_particles,))
-        self.v = ti.Vector.field(n=3, dtype=float, shape=(self.n_particles,))
-        self.J = ti.field(dtype=float, shape=(self.n_particles,))
-        self.C = ti.Matrix.field(n=3, m=3, dtype=float, shape=(self.n_particles,))
-        self.status = ti.field(dtype=int, shape=(self.n_particles,))
+        self.x = ti.Vector.field(n=3, dtype=float, shape=(self.n_particles_max,))
+        self.v = ti.Vector.field(n=3, dtype=float, shape=(self.n_particles_max,))
+        self.J = ti.field(dtype=float, shape=(self.n_particles_max,))
+        self.C = ti.Matrix.field(n=3, m=3, dtype=float, shape=(self.n_particles_max,))
+        self.status = ti.field(dtype=int, shape=(self.n_particles_max,))
         self.grid_v = ti.Vector.field(n=3, dtype=float, shape=(self.n_grids, self.n_grids, self.n_grids))
         self.grid_m = ti.field(dtype=float, shape=(self.n_grids, self.n_grids, self.n_grids))
 
-        # attribute cache
-        self.insert_index = self.n_particles - 1
-        self.insert_indexes = ti.field(dtype=int, shape=(self.n_insert_per_step,))
-
         # rendering parameters
-        self.save_frames = True
+        self.save_frames = False
         self.save_obj = False
         self.save_seconds = 10.0
         self.frame_rate = 1. / self.dt / self.n_steps
@@ -76,7 +89,13 @@ class MPM3DSimulator:
         self.reconstruct_resolution = (100, 100, 100)
         self.reconstruct_radius = 0.1
 
+        # boundary
         self.bounding_box_vertices = ti.Vector.field(n=3, dtype=float, shape=(24,))
+        self.n_spheres = 1
+        self.sphere_centers = [ti.Vector.field(n=3, dtype=float, shape=(1,)) for _ in range(self.n_spheres)]
+        self.sphere_radius = [0.15]
+        self.sphere_colors = [(0.8, 0.2, 0.2)]
+        self.int_cache = ti.field(dtype=int, shape=(1,))
 
     @ti.func
     def get_grid_index(self, xyz):
@@ -84,13 +103,30 @@ class MPM3DSimulator:
 
     @ti.kernel
     def initialize(self):
-        for index in ti.grouped(self.x):
-            self.x[index] = [ti.random() * 0.4 + 0.2, ti.random() * 0.4 + 0.2, ti.random() * 0.4 + 0.2]
+        self.sphere_centers[0][0] = [0.3, 0.15, 0.6]
+
         self.v.fill([0., 0., 0.])
         self.J.fill(1.)
         self.C.fill(0.)
-        self.status.fill(ParticleStatus.VALID)
-        self.radius_insert_last[0] = 0.
+        for index in range(self.n_particles):
+            self.status[index] = ParticleStatus.VALID
+            self.int_cache[0] = 1
+            while self.int_cache[0] > 0:
+                self.x[index] = [ti.random() * 0.4 + 0.2, ti.random() * 0.4 + 0.2, ti.random() * 0.4 + 0.2]
+                self.int_cache[0] = -1
+                for i in ti.static(range(self.n_spheres)):
+                    if in_sphere(self.x[index], self.sphere_centers[i][0], self.sphere_radius[i]):
+                        self.int_cache[0] = 1
+
+        for index in range(self.n_particles, self.n_particles_max):
+            theta = ti.random() * 2 * np.pi
+            x_offset = (ti.random() - 0.5) * self.v_insert[1] * self.dt * self.n_steps
+            r = 10000000.
+            while r > self.radius_insert:
+                r = ti.abs(gaussian_random()) * self.radius_insert
+            self.x[index] = [r * ti.cos(theta), 0., r * ti.sin(theta)] + self.pos_insert + x_offset
+            self.v[index] = self.v_insert
+            self.status[index] = ParticleStatus.INVALID
 
         bound_exact = (self.bound - 0.7) / self.n_grids
         pos = [bound_exact, 1. - bound_exact]
@@ -100,97 +136,16 @@ class MPM3DSimulator:
             self.bounding_box_vertices[index][(axis + 1) % 3] = pos[j]
             self.bounding_box_vertices[index][(axis + 2) % 3] = pos[k]
 
-
     @ti.kernel
     def clear_grid(self):
         self.grid_v.fill([0., 0., 0.])
         self.grid_m.fill(0.)
 
-    def reverse(self, size_new: int):
-        assert size_new >= self.n_particles
-        if size_new == self.n_particles:
-            return
-
-        self.x_new = ti.Vector.field(n=3, dtype=float, shape=(size_new,))
-        self.v_new = ti.Vector.field(n=3, dtype=float, shape=(size_new,))
-        self.J_new = ti.field(dtype=float, shape=(size_new,))
-        self.C_new = ti.Matrix.field(n=3, m=3, dtype=float, shape=(size_new,))
-        self.status_new = ti.field(dtype=int, shape=(size_new,))
-
-        self.x_new.fill([0., 0., 0.])
-        self.v_new.fill([0., 0., 0.])
-        self.J_new.fill(1.)
-        self.C_new.fill(0.)
-        self.status_new.fill(ParticleStatus.INVALID)
-
-        self.copy_from_new()
-
-        self.x = self.x_new
-        self.v = self.v_new
-        self.J = self.J_new
-        self.C = self.C_new
-        self.status = self.status_new
-        del self.x_new
-        del self.v_new
-        del self.J_new
-        del self.C_new
-        del self.status_new
-
-        self.n_particles = size_new
-
     @ti.kernel
-    def copy_from_new(self):
-        for index in ti.grouped(self.x):
-            if self.status[index] == ParticleStatus.VALID:
-                self.x_new[index] = self.x[index]
-                self.v_new[index] = self.v[index]
-                self.J_new[index] = self.J[index]
-                self.C_new[index] = self.C[index]
-                self.status_new[index] = self.status[index]
-
-    # @ti.kernel
-    # def valid_n(self) -> int:
-    #     n = ti.int32(0)
-    #     for i in ti.grouped(self.status):
-    #         if self.status[i] == ParticleStatus.VALID:
-    #             ti.atomic_add(n, 1)
-    #     return int(n)
-
-    def valid_n(self) -> int:
-        return self.n_valid
-
-    @ti.kernel
-    def init_added_particles(self):
-        for i in ti.grouped(self.insert_indexes):
-            index = self.insert_indexes[i]
-            theta = ti.random() * 2 * np.pi
-            x_offset = (ti.random() - 0.5) * self.v_init_insert[1] * self.dt * self.n_steps
-            r = 10000000.
-            while ti.abs(r - self.radius_insert_last[0]) > self.radius_insert_delta or r > self.radius_insert:
-                r = ti.abs(gaussian_random()) * self.radius_insert
-            self.radius_insert_last[0] = r
-            self.x[index] = [r * ti.cos(theta), 0., r * ti.sin(theta)] + self.pos_insert + x_offset
-            self.v[index] = self.v_init_insert
-            self.J[index] = 1.
-            self.C[index] = ti.Matrix.zero(float, 3, 3)
+    def add_particles(self, offset: int, n: int):
+        print('add particles ', offset, n)
+        for index in range(offset, offset + n):
             self.status[index] = ParticleStatus.VALID
-
-    def add_particles(self):
-        n = self.n_insert_per_step
-        if self.valid_n() + n > self.n_particles:
-            new_size = self.n_particles + max(n, self.n_particles, 1 << 20)
-            print(f'Reverse from {self.n_particles} to {new_size}', )
-            self.reverse(new_size)
-        insert_indexes_np = []
-        while n > 0:
-            while self.status[self.insert_index] != ParticleStatus.INVALID or self.insert_index in insert_indexes_np:
-                self.insert_index = (self.insert_index + 1) % self.n_particles
-            n -= 1
-            insert_indexes_np.append(self.insert_index)
-        insert_indexes_np = np.array(insert_indexes_np)
-        self.insert_indexes.from_numpy(insert_indexes_np)
-        self.init_added_particles()
-        self.n_valid += self.n_insert_per_step
 
     @ti.func
     def weight_kernel(self, x):
@@ -222,12 +177,27 @@ class MPM3DSimulator:
     @ti.kernel
     def simulate_grid(self):
         for i, j, k in ti.ndrange(self.grid_v.shape[0], self.grid_v.shape[1], self.grid_v.shape[2]):
+            # gravity
             m_temp = self.grid_m[i, j, k]
             v_temp = ti.select(m_temp > 0, self.grid_v[i, j, k] / m_temp, ti.Vector.zero(float, 3))
             v_temp[1] -= self.dt * self.gravity
-            index = ti.Vector([i, j, k], dt=int)
-            cond = (index < self.bound and v_temp < 0.) or (index > self.n_grids - self.bound and v_temp > 0.)
+
+            # bump into boundary
+            # axis aligned bounding box
+            grid_index = ti.Vector([i, j, k], dt=int)
+            cond = (grid_index < self.bound and v_temp < 0.) or (grid_index > self.n_grids - self.bound and v_temp > 0.)
             v_temp = ti.select(cond, 0., v_temp)
+
+            # object placed in the space
+            for index in ti.static(range(self.n_spheres)):
+                sphere_center = self.sphere_centers[index][0]
+                radius = self.sphere_radius[index]
+                grid_x = (grid_index.cast(float) + 0.5) * self.dx
+                normal = normal_sphere(grid_x, sphere_center)
+                beta = v_temp.dot(normal)
+                if in_sphere(grid_x, sphere_center, radius) and beta < 0.:
+                    v_temp -= beta * normal
+
             self.grid_v[i, j, k] = v_temp
 
     @ti.kernel
@@ -257,11 +227,13 @@ class MPM3DSimulator:
         self.grid_to_point()
 
     def step(self):
-        self.add_particles()
+        n = min(self.n_insert_per_step, self.n_particles_max - self.n_particles)
+        self.add_particles(self.n_particles, n)
+        self.n_particles += n
         for i in range(self.n_steps):
             self.substep()
 
-    def metaball_scalar_field(self, resolution):
+    def metasphere_scalar_field(self, resolution):
         dx = self.dx / resolution[0]
         dy = self.dx / resolution[1]
         dz = self.dx / resolution[2]
@@ -280,11 +252,11 @@ class MPM3DSimulator:
         y = j * dy
         z = k * dz
         field_pos = np.stack((x, y, z), axis=3)
-        particle_indices = kdtree.query_ball_point(field_pos, radius, workers=-1)
+        particle_indices = kdtree.query_sphere_point(field_pos, radius, workers=-1)
 
         f = np.zeros(f_shape)
 
-        def metaball_eval(p1, p2s):
+        def metasphere_eval(p1, p2s):
             if p2s is None:
                 return 0.0
             r = np.clip(np.linalg.norm(p1 - p2s, axis=1) / radius, 0.0, 1.0)
@@ -300,13 +272,13 @@ class MPM3DSimulator:
                     p1 = np.array([x[idx], y[idx], z[idx]])
                     p2s = np.array([particle_pos[p2_idx] for p2_idx in particle_indices[idx]]) if particle_indices[
                         idx] else None
-                    f[idx] = metaball_eval(p1, p2s)
+                    f[idx] = metasphere_eval(p1, p2s)
         return f
 
     def reconstruct_mesh(self, model_output_dir, frame_index):
         obj_name = os.path.join(model_output_dir, 'models', f'{frame_index:06d}.obj')
         print(obj_name)
-        f = self.metaball_scalar_field(self.reconstruct_resolution)
+        f = self.metasphere_scalar_field(self.reconstruct_resolution)
         print(f.shape)
         vertices, faces = mcubes.marching_cubes(f, self.reconstruct_threshold)
         print(vertices.shape, faces.shape)
@@ -321,10 +293,13 @@ class MPM3DSimulator:
                                 int(self.frame_rate + 0.5), self.resolution)
         for i, frame in enumerate(tqdm.tqdm(frames)):
             frame_image = cv2.imread(os.path.join(render_output_dir, frame), cv2.IMREAD_COLOR)
+            assert frame_image.shape[:2] == self.resolution
             video.write(frame_image)
         video.release()
 
-    def render_1spp(self, frame_index, window, scene, camera, canvas, model_output_dir, render_output_dir):
+    def render_1spp(self, frame_index: int,
+                    window: ti.ui.Window, scene: ti.ui.Scene, camera: ti.ui.Camera, canvas: ti.ui.Canvas,
+                    model_output_dir, render_output_dir):
         self.step()
 
         camera.position(2, 0.8, 1.6)
@@ -334,8 +309,10 @@ class MPM3DSimulator:
         scene.point_light(pos=(-1.2, 1.2, 2), color=(1, 1, 1))
         scene.ambient_light((0.5, 0.5, 0.5))
 
-        scene.particles(centers=self.x, radius=self.point_radius, color=self.point_color)
+        scene.particles(centers=self.x, radius=self.point_radius, color=self.point_color, index_count=self.n_particles)
         scene.lines(vertices=self.bounding_box_vertices, width=0.02, color=(1., 1., 0.))
+        for i in range(self.n_spheres):
+            scene.particles(centers=self.sphere_centers[i], radius=self.sphere_radius[i], color=self.sphere_colors[i])
 
         canvas.scene(scene)
 
@@ -345,16 +322,16 @@ class MPM3DSimulator:
         if self.save_obj:
             self.reconstruct_mesh(model_output_dir, frame_index)
 
-        window.show()
-
     def run(self, seconds=None):
-        window = ti.ui.Window('MPM3D', self.resolution, vsync=True)
+        print('Start initialization')
+        self.initialize()
+        print('Finish initialization')
+
+        window = ti.ui.Window('MPM3D', self.resolution, vsync=True, show_window=seconds is None)
         canvas = window.get_canvas()
         canvas.set_background_color(self.background_color)
         scene = ti.ui.Scene()
         camera = ti.ui.Camera()
-
-        self.initialize()
 
         output_dir = os.path.join(os.path.dirname(__file__), 'mpm3d-outputs')
 
@@ -369,12 +346,12 @@ class MPM3DSimulator:
             self.save_seconds = seconds
             for frame_index in tqdm.tqdm(range(int(self.save_seconds * self.frame_rate))):
                 self.render_1spp(frame_index, window, scene, camera, canvas, model_output_dir, render_output_dir)
+                window.show()
         else:
-            frame_index = int(self.save_seconds * self.frame_rate)
-            for frame_index in tqdm.tqdm(range(frame_index)):
-                self.render_1spp(frame_index, window, scene, camera, canvas, model_output_dir, render_output_dir)
+            frame_index = 0
             while window.running:
                 self.render_1spp(frame_index, window, scene, camera, canvas, model_output_dir, render_output_dir)
+                window.show()
                 frame_index += 1
 
         if self.save_frames:
@@ -383,5 +360,5 @@ class MPM3DSimulator:
 
 if __name__ == '__main__':
     mpm3d_simulator = MPM3DSimulator()
-    # mpm3d_simulator.run()
-    mpm3d_simulator.frames2mp4('mpm3d-outputs/renders')
+    mpm3d_simulator.run()
+    # mpm3d_simulator.frames2mp4('mpm3d-outputs/renders')
